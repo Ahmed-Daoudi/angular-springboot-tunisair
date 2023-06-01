56c83d1db535a97f2df0222734ea080b9ce32cc9
Ahmed-Daoudi <ahmeddaoudi386@gmail.com>
2023-06-01 17:45:13 -0400

    backend commit

---
+++ Tunisia_airplane/src/main/java/com/example/tunisia_airplane/TunisiaAirplaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.tunisia_airplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunisiaAirplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunisiaAirplaneApplication.class, args);
+	}
+
+}
---
+++ Tunisia_airplane/src/test/java/com/example/tunisia_airplane/TunisiaAirplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tunisia_airplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunisiaAirplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
